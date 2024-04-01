b16d395e7d0e50d3cdbd9af47e3cb975aba32b6c
CeriiZedF <dasbalan14@gmail.com>
2024-04-01 20:56:51 +0200

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SeeWorldComponent } from './see-world/see-world.component';
+import { DealsComponent } from './deals/deals.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SeeWorldComponent,
+    DealsComponent
   ],
   imports: [
     BrowserModule,
